4971572e61ad2910722fd29e7c2674ed6d47e0ae
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-04-07 01:00:57 +0600

    Basic Setup Done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactTooltip from "react-tooltip";
+
+import GitHubCalendar from "react-github-calendar";
+
+const Home = () => {
+  return (
+   <div>
+    Home
+   </div>
+  );
+};
+
+export default Home;
